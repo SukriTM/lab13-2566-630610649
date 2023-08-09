2ea0c572ff169d3929f93573f161bbeb6d95879e
SukriTM <micsu.ky@gmail.com>
2023-08-09 16:38:02 +0700

    Navbar+searchInput pagejs updated

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -3,7 +3,11 @@
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
-export default function SearchResultPage() {
+export default function SearchResultPage({ params }) 
+{  const searchInput = params.searchInput.replaceAll("%20", " ");
+  const filteredMovies = movieDB.filter((movie) =>
+  movie.title.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase())
+);
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
 
@@ -17,14 +21,26 @@ export default function SearchResultPage() {
     you code here...
   );
   */
-
+ 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+      Searching &quot; {searchInput} &quot;
+      </p>
+      <p className="fw-bold fs-4 text-center">
+        Found {filteredMovies.length} result(s)
       </p>
       <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {filteredMovies.map((movie, i) => (
+        <MovieRow
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          rating={movie.rating}
+          number={i + 1}
+        />
+      ))}
     </div>
   );
 }
